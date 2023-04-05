4b6a975939d91a3f16a4fb7da65b6f2e8e9e9019
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-04-05 20:32:44 -0300

    Atividades 05/04/2023
    Atividades feitas em sala

---
+++ Soma.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Soma{
+    public static void main (String[] args) {
+    //variaveis
+    int A, B, soma;
+    Scanner entrada;
+
+    //entrada
+    entrada = new Scanner(System.in);
+    A = entrada.nextInt();
+    B = entrada.nextInt();
+    entrada.close();
+
+    //processamento
+    soma = A + B;
+
+    //saida
+    System.out.printf("SOMA = %d", soma);
+
+    }
+
+}
+
---
+++ carro.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class carro{
+        public static void main (String[] args) {
+        //variaveis
+        float Carro_fabrica, Carro_novo, Distribuidor_porc, Imposto_porc;
+        Scanner entrada;
+
+        //entrada
+        entrada = new Scanner(System.in);
+        System.out.println("Digite o valor de fábrica do veículo: ");
+        Carro_fabrica = entrada.nextFloat();
+        System.out.println("Digite o percentual do Distribuidor do veículo: ");
+        Distribuidor_porc = entrada.nextFloat();
+        System.out.println("Digite o percentual do imposto ser pago do veículo: ");
+        Imposto_porc = entrada.nextFloat();
+        entrada.close();
+
+        //processamento
+        Carro_novo = (Float) Carro_fabrica * (Distribuidor_porc/100) + Carro_fabrica * (Imposto_porc/100) + Carro_fabrica;
+
+        //saida
+        System.out.println("-----------------------------------");
+        System.out.printf("O valor do veículo será de: R$ %.2f.\n", Carro_novo);
+        System.out.println("-----------------------------------");
+    }
+}
