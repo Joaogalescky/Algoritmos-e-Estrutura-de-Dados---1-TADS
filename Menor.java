9d049fa9a5dd686c62fdd049785c1b0768641699
Joaogalescky <joaocvgalescky@gmail.com>
2023-05-23 20:40:39 -0300

    Atividade

---
+++ Menor.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Menor {
+    public static void main(String[] args) {
+        
+        //variaveis
+        int n, repetidor;
+        Scanner entrada;
+
+        entrada = new Scanner(System.in);
+        System.out.println("Digite seus números positivos, digite 0 no final para ser mostrado o menor valor");
+
+        n = 1;
+        repetidor = Integer.MAX_VALUE;
+        // Integer.MAX_VALUE é o maior número permitido pelo computador, podendo atuar como repetidor.
+        System.out.printf("Numero maximo permitido: %d\n", repetidor);
+
+        while(n!=0){
+            n = entrada.nextInt();
+            if (n!=0 && n<repetidor){
+                repetidor = n;
+            }
+        }
+        entrada.close();
+        System.out.println("---------------------");
+        System.out.printf("O menor número é: %d\n", repetidor);
+        System.out.println("---------------------");
+    }
+}
