af4acfd7950330f0cf9e495d963c39b8a269d833
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-06-13 19:55:29 -0300

    Atv 13/06/2023 - Modularização (Metodo)
    Modularização em Portugal
    Metodo em Java

---
+++ atividades/Modularização.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Modularização{
+    public static void cabecalho() { //Em portugal, é uma modalização
+        System.out.println("+=+=+=+=+=+=+=+=+="); //Modulo
+    }
+    public static void main (String [] args){
+
+        //variaveis
+        String name;
+        Scanner entrada = new Scanner (System.in); //Novo metódo de entrada Scanner
+
+        //entrada e saida
+        cabecalho(); //Procedimento
+        System.out.println("Digite seu nome: ");
+        name = entrada.next();
+        entrada.close();
+        System.out.printf("Boa noite, %s\n", name);
+        cabecalho(); //Procedimento
+
+    }
+}
---
+++ atividades/Modularização2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Modularização2{
+
+    static Scanner entrada = new Scanner (System.in);
+
+    public static void cabecalho() { //Em portugal, é uma modalização
+        System.out.println("+=+=+=+=+=+=+=+=+="); //Modulo
+    }
+
+    public static String getString() { //No public static, o retorno será em "String" (Texto), diferente de "void"
+        String st;
+        st = entrada.next();
+
+        return st;
+    }
+
+    public static void main (String [] args){
+        //variaveis
+        String nome, sobrenome;
+
+        //entrada e saida
+        cabecalho(); //Procedimento
+        System.out.println("Digite seu nome: ");
+        nome = getString();
+        System.out.println("Digite seu sobrenome: ");
+        sobrenome = getString();
+        System.out.printf("Boa noite, %s %s\n", nome, sobrenome);
+        cabecalho(); //Procedimento
+    }
+}
