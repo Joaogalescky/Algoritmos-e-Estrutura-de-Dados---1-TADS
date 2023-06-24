07381a8a9e332024999b3a0dc93798464bdd0951
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-06-23 22:39:53 -0300

    Atv Recursividade
    Atividades realizadas em sala de aula sobre a Recursividade

---
+++ Recursiva.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+    class Recursiva{
+
+        public static void contagemDecrescente(int x) {
+            while(x>=0){
+                System.out.println(x);
+                x--;
+            }
+        }
+
+        public static void contagemDecrescenteRec(int x) {
+                System.out.println(x);
+                if(x>0){
+                    x--;
+                    contagemDecrescente(x);
+                }
+            }
+        
+        public static void main(String [] args) {
+            Scanner entrada = new Scanner (System.in);
+            int x = entrada.nextInt();
+            entrada.close();
+            
+            contagemDecrescenteRec(x);
+            }
+        }
---
+++ RecursivaIII.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+    class RecursivaIII{
+
+        public static int somaRec(int x) {
+            if(x>0)
+                return(x + somaRec(x-1));
+
+            return 0;
+        }
+
+        public static void main(String [] args) {
+            Scanner entrada = new Scanner (System.in);
+            int x = entrada.nextInt();
+            entrada.close();
+            
+            System.out.println(somaRec(x));
+            }
+        }
