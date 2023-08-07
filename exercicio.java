9da5887b780cd0d40fd967629ee89ab3fec71200
Joaogalescky <joaocvgalescky@gmail.com>
2023-08-07 18:23:03 -0300

    update - studying

--- exercicio.java
+++ exercicio.java
@@ -10,56 +10,53 @@ public class exercicio{
         15. Implemente uma função chamada distanciaCartesiana que recebe dois pares de números, que correspondem a pontos em um plano cartesiano com coordenadas x, y. A função deve retornar a distância entre esses dois pontos. Não é necessário arredondar valores.
     */
 
-    public static boolean pot2(int x) {
+    //public static boolean pot2(int x){
 
-        if (x <= 0) { //"x" não pode ser menor ou igual a 0
-            return false; //retorne false
-        }
-        return (x & (x - 1)) == 0; 
-    } // retorne true
-
-    public static void eh_pot2(int x) {
-
-        if (pot2(x)) {
-            System.out.printf("%d é uma potência de 2.\n", x);
-        } else {
-            System.out.printf("%d não é uma potência de 2.\n", x);
-        }
-    }
-
-    //public static void potN(int x, int n) {
-        
-        /*
-                    int x = 16;
-                    int n = 2;
-                    
-                    if (potN(x, n)) {
-                        System.out.println(x + " é uma potência de " + n);
-                    } else {
-                        System.out.println(x + " não é uma potência de " + n);
-                    }
-                }
-                
-                public static boolean potN(int x, int n) {
-                    if (x == 1) {
-                        return true;  // Qualquer número elevado a 0 é igual a 1.
-                    }
-                    
-                    for (int i = 1; i <= x; i++) {
-                        int result = (int) Math.pow(n, i);
-                        
-                        if (result == x) {
-                            return true;
-                        } else if (result > x) {
-                            break;  // Se o resultado da potência exceder x, não é uma potência de n.
-                        }
-                    }
-                    
-                    return false;
-                }
+    /*
+            if(x <= 0){ //"x" não pode ser menor ou igual a 0
+                return false; //retorne false
             }
-        */
-    //}
+            return (x & (x - 1)) == 0; //O "&" vai fazer a validação do retorno
+        } // retorne true
+
+        public static void eh_pot2(int x){
+
+            if(pot2(x)){
+                System.out.printf("%d é uma potência de 2.\n", x);
+            }else{
+                System.out.printf("%d não é uma potência de 2.\n", x);
+            }
+        }
+    */
+
+    public static void potN(int x, int n){
+        
+        x = 16;
+        n = 2;
+                    
+        if potN(x, n){
+            System.out.println(x + " é uma potência de " + n);
+            }else{
+                System.out.println(x + " não é uma potência de " + n);
+            }
+        }
+                
+        public static boolean potN(int x, int n){
+            if (x == 1){
+                return true;  // Qualquer número elevado a 0 é igual a 1.
+            }
+                    
+            for (int i = 1; i <= x; i++){
+                int result = (int) Math.pow(n, i);
+                        
+                if (result == x){
+                    return true;
+                }else if (result > x){
+                    break;  // Se o resultado da potência exceder x, não é uma potência de n.
+                }
+            }  
+        return false;
+    }
 
     //public static void distanciaCartesiana(int x, int y) {
 
@@ -92,6 +89,6 @@ public class exercicio{
         //y = entrada.nextInt();
         entrada.close();
 
-        eh_pot2(x);
+        //eh_pot2(x);
     }
 }
