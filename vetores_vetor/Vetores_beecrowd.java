0aef67b337ea46f360a772958c52639facf7fc62
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-08-21 11:21:00 -0300

    Update Vetores_beecrowd.java
    Inclusão da atv beecrowd_2310

--- vetores_vetor/Vetores_beecrowd.java
+++ vetores_vetor/Vetores_beecrowd.java
@@ -125,11 +125,102 @@ public class Vetores_beecrowd{
         for(int i = 0; i < 20; i ++){ //Sistema de repetição para saída do resultado.
             System.out.printf("N[%d] = %d\n", i, N[i]);
         }
+
+        public static double calculo_Percentual(int total_tentativas, int total_sucessos) {
+            if(total_tentativas == 0){
+                return 0;
+            }
+            return ((double) total_sucessos / total_tentativas) * 100;
+        }
+
+        public static void beecrowd_2310(){
+
+        //beecrowd_2310 - 
+        /*Explicação
+        Um treinador de voleibol gostaria de manter estatísticas sobre sua equipe. A cada jogo, seu auxiliar anota quantas tentativas de saques, bloqueios e ataques cada um de seus jogadores fez, bem como quantos desses saques, bloqueios e ataques tiveram sucesso (resultaram em pontos). Seu programa deve mostrar qual o percentual de saques, bloqueios e ataques do time todo tiveram sucesso. 
+        */
+
+        /*Entrada
+        A entrada é dada pelo número de jogadores N (1 ≤ N ≤ 100), seguido pelo nome de cada um dos jogadores. Abaixo do nome de cada jogador, seguem duas linhas com três inteiros cada. Na primeira linha S, B e A (0 ≤ S,B,A ≤ 10000) representam a quantidade de tentativas de saques, bloqueios e ataques e na segunda linha, S1, B1 e A1 (0 ≤ S1 ≤ S; 0 ≤ B1 ≤ B; 0 ≤ A1 ≤ A) com o número de saques, bloqueios e ataques deste jogador que tiveram sucesso.
+        */
+
+        /*Saída
+        A saída deve conter o percentual total de saques, bloqueios e ataques do time todo que resultaram em pontos, conforme mostrado no exemplo.
+        */
+
+        /*Exemplo
+        Entrada	
+        3
+        Renan
+        10 20 12
+        1 10 9
+        Jonas
+        8 7 1
+        2 7 0
+        Edson
+        3 3 3
+        1 2 3
     
+        Exemplo de Saída
+        Pontos de Saque: 19.05 %.
+        Pontos de Bloqueio: 63.33 %.
+        Pontos de Ataque: 75.00 %.
+        */
+            
+        //variáveis
+        Scanner entrada = new Scanner(System.in);
+        String nome;
+        int N;
+
+        double total_de_saques = 0, total_saques_sucesso = 0;
+        double total_de_bloqueios = 0, total_bloqueios_sucesso = 0;
+        double total_de_ataques = 0, total_ataques_sucesso = 0;
+
+        double percentual_sucesso_saques;
+        double percentual_sucesso_bloqueios;
+        double percentual_sucesso_ataques;
+
+        //entrada
+        N = entrada.nextInt();
+
+        //processamento
+        for (int i = 0; i < N; i++) {
+            nome = entrada.next();
+            int[] tentativas = new int[3];
+            int[] sucessos = new int[3];
+
+            for (int i2 = 0; i2 < 3; i2++) {
+                tentativas[i2] = entrada.nextInt();
+            }
+            for (int i2 = 0; i2 < 3; i2++) {
+                sucessos[i2] = entrada.nextInt();
+            }
+
+            total_de_saques += tentativas[0];
+            total_saques_sucesso += sucessos[0];
+            total_de_bloqueios += tentativas[1];
+            total_bloqueios_sucesso += sucessos[1];
+            total_de_ataques += tentativas[2];
+            total_ataques_sucesso += sucessos[2];
+        }
+        entrada.close();
+
+        //saida
+        percentual_sucesso_saques = calculo_Percentual((int) total_de_saques, (int) total_saques_sucesso);
+        percentual_sucesso_bloqueios = calculo_Percentual((int) total_de_bloqueios, (int) total_bloqueios_sucesso);
+        percentual_sucesso_ataques = calculo_Percentual((int) total_de_ataques, (int) total_ataques_sucesso);
+
+        System.out.printf("Pontos de Saque: %.2f %%.\n", percentual_sucesso_saques);
+        System.out.printf("Pontos de Bloqueio: %.2f %%.\n", percentual_sucesso_bloqueios);
+        System.out.printf("Pontos de Ataque: %.2f %%.\n", percentual_sucesso_ataques);
+    }
+}
+
     public static void main(String[] args) {
         //beecrowd_1174();
         //beecrowd_1180();
         //beecrowd_1177();
         //beecrowd_1175();
+        //beecrowd_2310();
     }
 }
