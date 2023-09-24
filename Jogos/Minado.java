a5082da4b958e97e703b5a138eda1ffd03529d88
Joaogalescky <joaocvgalescky@gmail.com>
2023-09-24 19:11:34 -0300

    update

--- Jogos/Minado.java
+++ Jogos/Minado.java
@@ -1,13 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Minado {
+public class Minado{
+    //variaveis
     private int tabuleiro_tamanho;
     private int quantidade_bombas;
     private char[][] tabuleiro;
     private char[][] tabuleiro_visivel;
     private boolean situacao_do_jogo;
-
+    
     public Minado(int tabuleiro_tamanho, int quantidade_bombas){
         this.tabuleiro_tamanho = tabuleiro_tamanho;
         this.quantidade_bombas = quantidade_bombas;
@@ -19,8 +20,9 @@ public class Minado {
     }
 
     private void Iniciar_tabuleiro(){
-        for (int i = 0; i < tabuleiro_tamanho; i++){
-            for (int j = 0; j < tabuleiro_tamanho; j++){
+        //Sistema de repetição: criando tabuleiro contendo as bombas
+        for(int i = 0; i < tabuleiro_tamanho; i++){
+            for(int j = 0; j < tabuleiro_tamanho; j++){
                 tabuleiro[i][j] = ' ';
                 tabuleiro_visivel[i][j] = '-';
             }
@@ -28,14 +30,17 @@ public class Minado {
     }
 
     private void Colocar_bombas(){
+        //variaveis
         Random randomico = new Random();
         int bombas_colocadas = 0;
 
-        while (bombas_colocadas < quantidade_bombas){
+        //Sistema de repetição: colocação das bombas
+        while(bombas_colocadas < quantidade_bombas){
             int x = randomico.nextInt(tabuleiro_tamanho);
             int y = randomico.nextInt(tabuleiro_tamanho);
 
-            if (tabuleiro[x][y] != '*'){
+            //Colocação das bombas
+            if(tabuleiro[x][y] != '*'){
                 tabuleiro[x][y] = '*';
                 bombas_colocadas++;
             }
@@ -44,8 +49,9 @@ public class Minado {
 
     private void Tabuleiro_visivel(){
         System.out.println("Tabuleiro:");
-        for (int i = 0; i < tabuleiro_tamanho; i++){
-            for (int j = 0; j < tabuleiro_tamanho; j++){
+        //Sistema de repetição: criando tabuleiro visivel ao usuário
+        for(int i = 0; i < tabuleiro_tamanho; i++){
+            for(int j = 0; j < tabuleiro_tamanho; j++){
                 System.out.print(tabuleiro_visivel[i][j] + " ");
             }
             System.out.println();
@@ -54,21 +60,22 @@ public class Minado {
 
     public void Jogar(){
         Scanner entrada = new Scanner(System.in);
+        int linha, coluna;
 
-        while (situacao_do_jogo){
+        while(situacao_do_jogo){
             Tabuleiro_visivel();
             System.out.print("Digite a linha (0 a " + (tabuleiro_tamanho - 1) + "): ");
-            int linha = entrada.nextInt();
+            linha = entrada.nextInt();
 
             System.out.print("Digite a coluna (0 a " + (tabuleiro_tamanho - 1) + "): ");
-            int coluna = entrada.nextInt();
+            coluna = entrada.nextInt();
 
-            if (linha < 0 || linha >= tabuleiro_tamanho || coluna < 0 || coluna >= tabuleiro_tamanho){
+            if(linha < 0 || linha >= tabuleiro_tamanho || coluna < 0 || coluna >= tabuleiro_tamanho){
                 System.out.println("Coordenadas inválidas. Tente novamente.");
                 continue;
             }
 
-            if (tabuleiro[linha][coluna] == '*'){
+            if(tabuleiro[linha][coluna] == '*'){
                 System.out.println("Você perdeu! Uma bomba explodiu.");
                 situacao_do_jogo = false;
             }else{
@@ -79,20 +86,22 @@ public class Minado {
         entrada.close();
     }
 
-    private void Descobrir_celula(int linha, int coluna) {
-        if (tabuleiro_visivel[linha][coluna] != '-') {
+    private void Descobrir_celula(int linha, int coluna){
+        int bombas_adjacentes, i, j;
+
+        if(tabuleiro_visivel[linha][coluna] != '-'){
             System.out.println("Esta célula já foi descoberta.");
             return;
         }
 
-        int bombas_adjacentes = Contar_bombas_adjacentes(linha, coluna);
+        bombas_adjacentes = Contar_bombas_adjacentes(linha, coluna);
         tabuleiro_visivel[linha][coluna] = (char) (bombas_adjacentes + '0');
 
-        if (bombas_adjacentes == 0) {
+        if(bombas_adjacentes == 0){
             // Se não houver bombas adjacentes, revelar células vizinhas recursivamente.
-            for (int i = linha - 1; i <= linha + 1; i++) {
-                for (int j = coluna - 1; j <= coluna + 1; j++) {
-                    if (i >= 0 && i < tabuleiro_tamanho && j >= 0 && j < tabuleiro_tamanho) {
+            for(i = linha - 1; i <= linha + 1; i++){
+                for(j = coluna - 1; j <= coluna + 1; j++){
+                    if(i >= 0 && i < tabuleiro_tamanho && j >= 0 && j < tabuleiro_tamanho){
                         Descobrir_celula(i, j);
                     }
                 }
@@ -100,39 +109,40 @@ public class Minado {
         }
     }
 
-    private int Contar_bombas_adjacentes(int linha, int coluna) {
+    private int Contar_bombas_adjacentes(int linha, int coluna){
         int bombas_adjacentes = 0;
+        int i, j;
 
-        for (int i = linha - 1; i <= linha + 1; i++) {
-            for (int j = coluna - 1; j <= coluna + 1; j++) {
-                if (i >= 0 && i < tabuleiro_tamanho && j >= 0 && j < tabuleiro_tamanho && tabuleiro[i][j] == '*') {
+        for(i = linha - 1; i <= linha + 1; i++){
+            for(j = coluna - 1; j <= coluna + 1; j++){
+                if(i >= 0 && i < tabuleiro_tamanho && j >= 0 && j < tabuleiro_tamanho && tabuleiro[i][j] == '*'){
                     bombas_adjacentes++;
                 }
             }
         }
-
         return bombas_adjacentes;
     }
 
-    private void Fim_de_jogo() {
+    private void Fim_de_jogo(){
         boolean vitoria = true;
+        int i, j;
 
-        for (int i = 0; i < tabuleiro_tamanho; i++) {
-            for (int j = 0; j < tabuleiro_tamanho; j++) {
-                if (tabuleiro[i][j] != '*' && tabuleiro_visivel[i][j] == '-') {
+        for(i = 0; i < tabuleiro_tamanho; i++){
+            for(j = 0; j < tabuleiro_tamanho; j++){
+                if(tabuleiro[i][j] != '*' && tabuleiro_visivel[i][j] == '-'){
                     vitoria = false;
                     break;
                 }
             }
         }
 
-        if (vitoria) {
+        if(vitoria){
             System.out.println("Parabéns! Você ganhou o jogo.");
             situacao_do_jogo = false;
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int tamanho_do_tabuleiro = 10, numero_de_bombas = 15;
 
         Minado campo_minado = new Minado(tamanho_do_tabuleiro, numero_de_bombas);
