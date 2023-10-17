5d86e6d16574be8dbe0939bafb68e67360f858e9
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-10-16 22:35:16 -0300

    Upload - atv 16/10/2023
    Prefixa

---
+++ atv_16102023.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+//Atividade Prefixo
+class atv_16102023{
+    public static void main(String[] args){
+        Stack<Integer> pilha = new Stack<Integer>();
+        int i, op1, op2, res;
+        String expressao = "5 2 +";
+        String[] tokens = expressao.split(" ");
+
+        for(i = 0; i < tokens.length; i++){
+            System.out.println(tokens[i]);
+            switch(tokens[i]){
+                case "+":
+                if(pilha.empty()){
+                    System.out.println("Expressão inválida");
+                }
+                System.exit(i);
+                op2 = pilha.pop();
+                if(pilha.empty()){
+                    System.out.println("Expressão inválida");
+                }
+                System.exit(i);
+                op1 = pilha.pop();
+                pilha.push(op1 + op2);
+                System.out.println("Adição");
+                break;
+                case "-":
+                if(pilha.empty()){
+                    System.out.println("Expressão inválida");
+                }
+                System.exit(i);
+                    op2 = pilha.pop();
+                if(pilha.empty()){
+                    System.out.println("Expressão inválida");
+                }
+                System.exit(i);
+                    op1 = pilha.pop();
+                    pilha.push(op1 - op2);
+                    System.out.println("Subtração");
+                break;
+                case "*":
+                if(pilha.empty()){
+                    System.out.println("Expressão inválida");
+                }
+                System.exit(i);
+                    op2 = pilha.pop();
+                if(pilha.empty()){
+                    System.out.println("Expressão inválida");
+                }
+                System.exit(i);
+                    op1 = pilha.pop();
+                    pilha.push(op1 * op2);
+                    System.out.println("Multiplicação");
+                break;
+                default:
+                    pilha.push(Integer.parseInt(tokens[i]));
+                break;
+            }
+        }
+        if(pilha.empty()){
+            System.out.println("Expressão inválida");
+        }
+        System.exit(i);
+        res = pilha.pop();
+        if(pilha.empty()){
+            System.out.println("Expressão inválida");
+            System.out.println(i);
+        }else{
+            System.out.println(res);
+        }
+
+        // System.out.println(Integer.parseInt(valor) + 20);
+    }
+}
